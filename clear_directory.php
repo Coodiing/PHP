9637fbd8efd7fd7bbda15783e7e9c709e4a11976
harddesign <gregoire.billet@gmail.com>
2021-01-20 11:51:32 +0100

    Update clear_directory.php

--- clear_directory.php
+++ clear_directory.php
@@ -1,1 +1,21 @@
 
+* Clear the specific $folder (recursive) */
+function clear_directory($folder) {
+$open=@opendir($folder);
+if (!$open) return;
+while($file=readdir($open)) {
+    if ($file == "." || $file == "..") continue;
+    if (is_dir("$folder/$file")) {
+        $r=clear_directory("$folder/$file");
+        if (!$r) return false;
+    }
+    else {
+    	$r=@unlink("$folder/$file");
+    	if (!$r) return false;
+    }
+}
+closedir($open);
+$r=@rmdir($folder);
+if (!$r) return false;
+return true;
+}
