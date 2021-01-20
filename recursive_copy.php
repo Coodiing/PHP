5ec316c5b4bb0cd60fc931bb80e13007ebfd0d1e
harddesign <gregoire.billet@gmail.com>
2021-01-20 11:36:30 +0100

    Update recursive_copy.php

--- recursive_copy.php
+++ recursive_copy.php
@@ -1,1 +1,13 @@
 
+/* Reursive copy of $src folder to $dst */
+function recursive_copy($src,$dst) {
+$dir = opendir($src);
+@mkdir($dst);
+while(false !== ( $file = readdir($dir)) ) {
+    if ($file != "." && $file != "..") {
+        if (is_dir("$src/$file")) { recursive_copy("$src/$file","$dst/$file");}
+        else { copy("$src/$file","$dst/$file");}
+    }
+}
+closedir($dir);
+}
