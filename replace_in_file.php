36540e150f9a9a61024c83c4c2d422ca9cb1a059
harddesign <gregoire.billet@gmail.com>
2021-01-20 11:45:10 +0100

    Update replace_in_file.php

--- replace_in_file.php
+++ replace_in_file.php
@@ -1,4 +1,10 @@
 
-function replace {
-
-} 
+/* Replace $str with $newstr in $file */
+function replace_in_file($str,$newstr,$file)
+{
+    $content = file_get_contents($file);
+    $content = str_replace($str,$newstr,$content);
+    $file = fopen($file,"w+");
+    fwrite($file,$content);
+    fclose($file);
+}
